ecf1b36a8014cd751bc411ed7cb1ad485fe9131f
Behzad Golshan <behzad@recruit.ai>
2017-09-13 13:56:10 -0700

    Update version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='pykoko',
-    version='0.1.0',
+    version='0.1.1',
     description="KOKO is an easy-to-use entity extraction tool",
     long_description=readme + '\n\n' + history,
     author="BigGorilla Team",
